68a1ff10d6f4640e0a460a367fb6ac5154c815e1
Diana <diana.amfreitas@gmail.com>
2020-09-15 15:18:29 +0100

    Improving shape canvas mouse movement

--- script.js
+++ script.js
@@ -2,43 +2,57 @@
 let canvas = document.querySelector('canvas');
 let dpi = window.devicePixelRatio;
 let ctx = canvas.getContext('2d');
+let rect = canvas.getBoundingClientRect();
 
 let redShape = new Image();
 let blueShape = new Image()
 let yellowShape = new Image();
-redShape.src = 'icons/redShape.png';
-yellowShape.src = 'icons/yellowShape.png';
-blueShape.src = 'icons/blueShape.png';
 
-fix_dpi();
-redShape.onload = function(){
-    let ratio = redShape.height/redShape.width;
-    let newWidth = canvas.width*0.7;
-    let newHeight = newWidth * ratio;
-
-    drawImageWithSize(redShape, canvas.width/2 - newWidth/2 , 0, newWidth, newHeight);
-}
-yellowShape.onload = function(){
-    let ratio = yellowShape.height/yellowShape.width;
-    let newWidth = canvas.width*0.35;
-    let newHeight = newWidth * ratio;
-
-    let x = screen.width/2 - newWidth;
-
-    drawImageWithSize(yellowShape, x, canvas.height - newHeight, newWidth, newHeight);
-}
-blueShape.onload = function(){
-    let ratio = blueShape.height/blueShape.width;
-    let newWidth = canvas.width*0.35;
-    let newHeight = newWidth * ratio;
-
-    let x = screen.width/2 + newWidth/2;
-    
-    drawImageWithSize(blueShape, x , canvas.height - newHeight, newWidth, newHeight);
-}
+// wait for the content of the window element 
+// to load, then performs the operations. 
+// This is considered best practice. 
+window.addEventListener('load', ()=>{
+    canvas.addEventListener('mousemove', eraseShapes);
+    fix_dpi();
+    loadShapes();
+});
 
 function drawImageWithSize(img, x, y, width, height) {
   ctx.drawImage(img, x, y, width, height);
+}
+
+function loadShapes(){
+    redShape.src = 'icons/redShape.png';
+    yellowShape.src = 'icons/yellowShape.png';
+    blueShape.src = 'icons/blueShape.png';
+
+    redShape.onload = function(){
+        let ratio = redShape.height/redShape.width;
+        let newWidth = canvas.width*0.7;
+        let newHeight = newWidth * ratio;
+    
+        drawImageWithSize(redShape, canvas.width/2 - newWidth/2 , 0, newWidth, newHeight);
+    }
+
+    yellowShape.onload = function(){
+        let ratio = yellowShape.height/yellowShape.width;
+        let newWidth = canvas.width*0.35;
+        let newHeight = newWidth * ratio;
+    
+        let x = screen.width/2 - newWidth;
+    
+        drawImageWithSize(yellowShape, x, canvas.height - newHeight, newWidth, newHeight);
+    }
+    
+    blueShape.onload = function(){
+        let ratio = blueShape.height/blueShape.width;
+        let newWidth = canvas.width*0.35;
+        let newHeight = newWidth * ratio;
+    
+        let x = screen.width/2 + newWidth/2;
+        
+        drawImageWithSize(blueShape, x , canvas.height - newHeight, newWidth, newHeight);
+    }
 }
 
 function fix_dpi() {
@@ -47,7 +61,18 @@ function fix_dpi() {
    
     canvas.setAttribute('height', style_height * dpi);
     canvas.setAttribute('width', style_width * dpi);
-    }
+}
+
+function eraseShapes(e){
+    let x = (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+    let y = (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
+    let radius = 160;
+    ctx.globalCompositeOperation = 'destination-out';
+
+    ctx.beginPath();    
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);  
+    ctx.fill();
+}
 
 // Sticky Header and Divs On Scroll
 let header = document.querySelector('header');
