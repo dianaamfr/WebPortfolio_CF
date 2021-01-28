a1ca515fa3b52ccbda7dffd0ee6dc12cfe2f4408
Diana Freitas <up201806230@fe.up.pt>
2021-01-28 17:06:21 +0000

    Database update - details about each project

--- database/db_content.php
+++ database/db_content.php
@@ -3,14 +3,18 @@ include_once('database/db_connection.php');
 
 function getProjects(){
     $db = Database::instance()->db();
-    $stmt = $db->prepare("SELECT projectId, title, projectType FROM Project");
+    $stmt = $db->prepare("SELECT projectId, title, projectType 
+                          FROM Project");
     $stmt->execute();
     return $stmt->fetchAll();
 }
 
 function getProjectFirstImages($projectId){
     $db = Database::instance()->db();
-    $stmt = $db->prepare("SELECT imageOrder, description FROM Image WHERE projectId = ? LIMIT 3");
+    $stmt = $db->prepare("SELECT imageOrder, description FROM Image 
+                          WHERE projectId = ? 
+                          ORDER BY imageOrder ASC 
+                          LIMIT 3");
     $stmt->execute(array($projectId));
     return $stmt->fetchAll();
 }
